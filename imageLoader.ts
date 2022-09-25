5a61407500efffc6d905d36ae374aa4481cf23d2
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2022-09-25 09:27:44 +0200

    Page ready to build

---
+++ imageLoader.ts
@@ -0,0 +1,6 @@
+// Needed for 'next export'
+const imageLoader = ({ src }: { src: string }) => {
+  return src;
+};
+
+export default imageLoader;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // Allows images from these domains:
+  images: {
+    domains: ["rickandmortyapi.com"],
+    // For imageLoader
+    loader: "custom",
+    path: "/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
